c4815da46c4610b2ce2c28d790e5467b4113b482
Loic Jeannin <inklo.ca@gmail.com>
2024-10-07 07:39:47 -0400

    finish generator

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
+const gbutton = document.querySelector(".gbutton");
 const dataLowercase = "qwertyuiopasdfghjklzxcvbnm";
 const dataUppercase = dataLowercase.toUpperCase();
 const dataNumbers = "1234567890";
 const dataSymbols = "~!@#$%^&*()_+{}|:";
+const rangeValue = document.getElementById('password-length');
+const passwordOutput = document.getElementById('password-output');
+
 
 function generatePassword() {
+  let data = [ ];
+  let password = "";
 
-}
+  if(lowercase.checked) data.push(...dataLowercase);
+  if(uppercase.checked) data.push(...dataUppercase);
+  if(number.checked) data.push(...dataNumbers);
+  if(symbols.checked) data.push(...dataSymbols);
 
-generateButton.addEventListener("click", () => {
-  console.log("test");
-});
+  if (data.length === 0) {
+    alert("select range and options to generate a password");
+  return;
+  }
+
+  for(i = 0; i < rangeValue.value; i++) {
+    password += data[Math.floor(Math.random() * data.length)];
+    console.log(password);}
+
+    // on ne peut pas faire de innerHtml ou textContent sur un input
+  passwordOutput.value = password;
+};
+
+gbutton.addEventListener("click",generatePassword);
